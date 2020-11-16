7f7170a3e4bbebca3bf824977a9671159d35524f
zyx <zhaoyuxin124@gmail.com>
2020-11-15 21:31:08 -0500

    add vue front-end framework

---
+++ src/views/modules/product/category.vue
@@ -0,0 +1,28 @@
+<template>
+  <el-tree :data="data" :props="defaultProps" @node-click="handleNodeClick"></el-tree>
+</template>
+<script>
+export default {
+  components: {},
+  props: {},
+  data() {},
+ 
+  computed: {},
+  watch: {},
+  methods: {
+      handleNodeClick(data) {
+        console.log(data);
+      }
+    },
+  created() {},
+  mounted() {},
+  beforeCreate() {},
+  beforeMount() {},
+  beforeUpdate() {},
+  updated() {},
+  beforeDestroy() {},
+  destroyed() {},
+  activated() {},
+};
+</script>
+<style scoped></style>
