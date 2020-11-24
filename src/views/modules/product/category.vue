a90963e5ef4cbec9091819da8c01dab29366626f
zyx <zhaoyuxin124@gmail.com>
2020-11-24 10:42:01 -0500

    add the drag feature

--- src/views/modules/product/category.vue
+++ src/views/modules/product/category.vue
@@ -1,20 +1,265 @@
+/* eslint-disable */
 <template>
-  <el-tree :data="data" :props="defaultProps" @node-click="handleNodeClick"></el-tree>
+  <div>
+    <el-tree
+      :data="menus"
+      :props="defaultProps"
+      @node-click="handleNodeClick"
+      :expand-on-click-node="false"
+      show-checkbox
+      node-key="catId"
+      :default-expanded-keys="expandedKey"
+      draggable
+      :allow-drop="allowDrop"
+    >
+      <span class="custom-tree-node" slot-scope="{ node, data }">
+        <span>{{ node.label }}</span>
+        <span>
+          <el-button
+            v-if="node.level <= 2"
+            type="text"
+            size="mini"
+            @click="() => append(data)"
+          >
+            Append
+          </el-button>
+          <el-button type="text" size="mini" @click="() => edit(data)">
+            Edit
+          </el-button>
+          <el-button
+            v-if="node.childNodes.length == 0"
+            type="text"
+            size="mini"
+            @click="() => remove(node, data)"
+          >
+            Delete
+          </el-button>
+        </span>
+      </span>
+    </el-tree>
+
+    <el-dialog
+      :title="title"
+      :visible.sync="dialogVisible"
+      width="30%"
+      :before-close="handleClose"
+      :close-on-click-modal="false"
+    >
+      <el-form :model="category">
+        <el-form-item label="Category name" :label-width="formLabelWidth">
+          <el-input v-model="category.name" autocomplete="off"></el-input>
+        </el-form-item>
+        <el-form-item label="Icon" :label-width="formLabelWidth">
+          <el-input v-model="category.icon" autocomplete="off"></el-input>
+        </el-form-item>
+        <el-form-item label="Unit" :label-width="formLabelWidth">
+          <el-input
+            v-model="category.productUnit"
+            autocomplete="off"
+          ></el-input>
+        </el-form-item>
+      </el-form>
+
+      <span slot="footer" class="dialog-footer">
+        <el-button @click="dialogVisible = false">Cancel</el-button>
+        <el-button type="primary" @click="submitData">Confirm</el-button>
+      </span>
+    </el-dialog>
+  </div>
 </template>
 <script>
+import Vue from "vue";
 export default {
   components: {},
   props: {},
-  data() {},
- 
+  data() {
+    return {
+      maxLevel: 0,
+      title: "",
+      dialogType: "",
+      category: {
+        name: "",
+        parentCid: 0,
+        catLevel: 0,
+        showStatus: 1,
+        sort: 0,
+        catId: null,
+        productUnit: "",
+        icon: "",
+      },
+      menus: [],
+      expandedKey: [],
+      dialogVisible: false,
+      defaultProps: {
+        children: "children",
+        label: "name",
+      },
+    };
+  },
+
   computed: {},
   watch: {},
   methods: {
-      handleNodeClick(data) {
-        console.log(data);
+    getMenus() {
+      this.$http({
+        url: this.$http.adornUrl("/products/category/list/tree"),
+        method: "get",
+      }).then(({ data }) => {
+        if (data && data.code === 0) {
+          console.log("success getting data...", data.data);
+          this.menus = data.data;
+        } else {
+          this.dataList = [];
+          this.totalPage = 0;
+        }
+        this.dataListLoading = false;
+      });
+    },
+    edit(data) {
+      console.log("data: ", data);
+      this.dialogType = "edit";
+      this.title = "Edit a category";
+      this.dialogVisible = true;
+
+      // send requests to get the updated data
+      this.$http({
+        url: this.$http.adornUrl(`/products/category/info/${data.catId}`),
+        method: "get",
+      }).then(({ data }) => {
+        //success
+        console.log("data", data);
+        this.category.name = data.data.name;
+        this.category.catId = data.data.catId;
+        this.category.icon = data.data.icon;
+        this.category.productUnit = data.data.productUnit;
+        this.category.parentCid = data.data.parentCid;
+        this.category.catLevel = data.data.catLevel;
+        this.category.sort = data.data.sort;
+        this.category.showStatus = data.data.showStatus;
+      });
+    },
+    append(data) {
+      console.log("append", data);
+      this.dialogType = "add";
+      this.title = "Add a category";
+      this.dialogVisible = true;
+      this.category.parentCid = data.catId;
+      this.category.catLevel = data.catLevel * 1 + 1;
+
+      this.category.catId = data.catId;
+      this.category.icon = "";
+      this.category.productUnit = "";
+      this.category.sort = 0;
+      this.category.showStatus = 1;
+      this.category.name = "";
+    },
+    addCategory() {
+      console.log("data: ", this.category);
+      this.$http({
+        url: this.$http.adornUrl("/products/category/save"),
+        method: "post",
+        data: this.$http.adornData(this.category, false),
+      }).then(({ data }) => {
+        this.$message({
+          message: "Successfully save the menu!",
+          type: "success",
+        });
+        // close the dialog
+        this.dialogVisible = false;
+        // refresh new menu
+        this.getMenus();
+        // set the expanded menu
+        this.expandedKey = [this.category.parentCid];
+      });
+    },
+    editCategory() {
+      var { catId, name, icon, productUnit } = this.category;
+
+      this.$http({
+        url: this.$http.adornUrl("/products/category/update"),
+        method: "post",
+        data: this.$http.adornData({ catId, name, icon, productUnit }, false),
+      }).then(({ data }) => {
+        this.$message({
+          message: "successful edit",
+          type: "success",
+        });
+        // close the dialog
+        this.dialogVisible = false;
+        // refresh new menu
+        this.getMenus();
+        // set the expanded menu
+        this.expandedKey = [this.category.parentCid];
+      });
+    },
+    remove(node, data) {
+      var ids = [data.catId];
+      // pop up warning
+      this.$confirm(
+        `This will permanently delete ${data.name}. Continue?`,
+        "Warning",
+        {
+          confirmButtonText: "OK",
+          cancelButtonText: "Cancel",
+          type: "warning",
+        }
+      )
+        .then(() => {
+          this.$http({
+            url: this.$http.adornUrl("/products/category/delete"),
+            method: "post",
+            data: this.$http.adornData(ids, false),
+          }).then(({ data }) => {
+            this.$message({
+              message: "Successfully delete the menu!",
+              type: "success",
+            });
+            // refresh the update menu
+            this.getMenus();
+            // expand the menu
+            this.expandedKey = [node.parent.data.catId];
+          });
+        })
+        .catch(() => {});
+    },
+    allowDrop(draggingNode, dropNode, type) {
+      console.log("allow drop: ", draggingNode, dropNode, type)
+      this.maxLevel = 0
+      this.countNodeLevel(draggingNode.data)
+      let depth = this.maxLevel - draggingNode.level + 1
+      console.log("depth: ", depth)
+      console.log("dorpnode: ", dropNode.level)
+
+      if (type == "inner") {
+        return (depth + dropNode.level) <= 3
+      } else {
+        return (depth + dropNode.parent.level) <= 3;
       }
     },
-  created() {},
+    countNodeLevel(node) {
+      // find child nodes and get the maximum depth
+      if (node.children != null && node.children.length > 0) {
+        for (var i = 0; i < node.children.length; i++) {
+          if (node.children[i].catLevel > this.maxLevel) {
+            this.maxLevel = node.children[i].catLevel
+          }
+          this.countNodeLevel(node.children[i])
+        }
+      }
+    },
+    submitData(data) {
+      if (this.dialogType == "add") {
+        this.addCategory();
+      }
+      if (this.dialogType == "edit") {
+        this.editCategory();
+      }
+    },
+  },
+
+  created() {
+    this.getMenus();
+  },
   mounted() {},
   beforeCreate() {},
   beforeMount() {},
